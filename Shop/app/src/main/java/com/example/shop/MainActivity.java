3d82952bbab7287b9ea017a018e6be58ff884c9a
Vadim <vadimmm120@yandex.ru>
2019-12-14 21:12:56 +0300

    StacksBegin

--- Shop/app/src/main/java/com/example/shop/MainActivity.java
+++ Shop/app/src/main/java/com/example/shop/MainActivity.java
@@ -145,6 +145,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.stack:
                     i = new Intent(MainActivity.this, Stacks.class);
                     startActivity(i);
+                    i.putExtra("apple", apple.getQuantity());
+                    i.putExtra("cucumber", cucumber.getQuantity());
+                    i.putExtra("tomato", tomato.getQuantity());
                     break;
                 default:
                     break;
--- Shop/app/src/main/java/com/example/shop/Stacks.java
+++ Shop/app/src/main/java/com/example/shop/Stacks.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Stacks extends AppCompatActivity {
+
+    int appleC,tomatoC,cucumberC;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getIntent().getIntExtra("apple",appleC);
+        getIntent().getIntExtra("cucumber",cucumberC);
+        getIntent().getIntExtra("tomato",tomatoC);
     }
 }

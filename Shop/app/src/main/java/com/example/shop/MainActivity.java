9f5f11d25989cc0a90d4ffac521ecb137d6bcd3b
Krasav4ik <zub4encko.kostya@yandex.ru>
2019-12-14 16:08:20 +0300

    Shop_v.1.2
    Added randomization of customers

--- Shop/app/src/main/java/com/example/shop/MainActivity.java
+++ Shop/app/src/main/java/com/example/shop/MainActivity.java
@@ -2,21 +2,73 @@ package com.example.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView Welcome;
+    Button Start, Stack;
+    TextView Customer1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        Customer1 = (TextView) findViewById(R.id.Customer1);
+        Start = (Button) findViewById(R.id.Start);
+        Stack = (Button) findViewById(R.id.stack);
 
+
+   View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i;
+            switch (v.getId()) {
+                case R.id.Start:
+                    int MAX_CUSTOMERS;
+                    MAX_CUSTOMERS = (int)((Math.random() * (5)) + 1);
+                    /*
+                    Customer1.setText("" + MAX_CUSTOMERS);
+                    Проверка работы рандома
+                    */
+
+                    switch (MAX_CUSTOMERS){
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+                            
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+
+                            break;
+                    }
+                    break;
+                case R.id.stack:
+                    i = new Intent(MainActivity.this, Stacks.class);
+                    startActivity(i);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+        Start.setOnClickListener(listener);
     }
-    }
+}
+
+
+
+
 

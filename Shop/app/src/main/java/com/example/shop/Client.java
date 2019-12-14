35b8cfcd38a87aeb03ccf4824940aab40ec5cc90
Artem <artemis.gorb.14@gmail.com>
2019-12-14 20:04:55 +0300

    client changes

--- Shop/app/src/main/java/com/example/shop/Client.java
+++ Shop/app/src/main/java/com/example/shop/Client.java
@@ -1,5 +1,6 @@
 package com.example.shop;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Client extends Thread {
@@ -8,14 +9,70 @@ public class Client extends Thread {
     int maxProducts;
     String[] productList;
     Product nextProduct;
+    Tomato tomato;
+    Apple apple;
+    Cucumber cucumber;
 
     public Client(CashBox cash, int maxProducts){
         cashBox = cash;
         this.maxProducts = maxProducts;
+        this.productList = new String[maxProducts];
     }
 
     @Override
     public void run(){
+        int count = 0;
+        Random random = new Random();
+
+        while (this.productList[maxProducts - 1] == null){
+            int x = random.nextInt() % 3 + 1;
+            if(x == 1){
+                synchronized (tomato) {
+                    if(tomato.getQuantity() > 0) {
+                        try {
+                            TimeUnit.SECONDS.sleep(2);
+                            nextProduct = new Tomato();
+                            productList[count] = "Tomato";
+                            count++;
+                            moneyToPay += Tomato.price;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }else if(x == 2){
+                synchronized (cucumber) {
+                    if(cucumber.getQuantity() > 0) {
+                        try {
+                            TimeUnit.SECONDS.sleep(2);
+                            nextProduct = new Cucumber();
+                            productList[count] = "Cucumber";
+                            count++;
+                            moneyToPay += Cucumber.price;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }else {
+                synchronized (apple) {
+                    if(apple.getQuantity() > 0) {
+                        try {
+                            TimeUnit.SECONDS.sleep(2);
+                            nextProduct = new Apple();
+                            productList[count] = "Apple";
+                            count++;
+                            moneyToPay += Apple.price;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+
+
         synchronized (cashBox){
             if(cashBox.getStatus()){
                 try {
@@ -35,8 +92,6 @@ public class Client extends Thread {
             }
 
         }
-
-
     }
 
 }

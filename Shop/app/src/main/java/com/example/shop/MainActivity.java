dcac9758b5df8337881842f9d685a016acb89abd
Krasav4ik <zub4encko.kostya@yandex.ru>
2019-12-14 03:03:20 +0300

    Shop_v.1.1
    Main view with static customers

--- Shop/app/src/main/java/com/example/shop/MainActivity.java
+++ Shop/app/src/main/java/com/example/shop/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView Welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
-}
+    }
+
